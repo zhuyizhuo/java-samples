259881a90b93287a13e7dbae57c62d4f61e088f2
zhuyizhuo <2361883887@qq.com>
2021-01-28 22:39:51 +0800

    :tada:  新增 leetcode 题解
    :tada:  新增 leetcode 题解

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems724/FindPivotIndex.java
@@ -0,0 +1,35 @@
+package com.github.zhuo.algorithm.leetcode.problems724;
+
+/**
+ * https://leetcode-cn.com/problems/find-pivot-index/
+ *
+ * 寻找数组的中心索引
+ */
+public class FindPivotIndex {
+
+    public static void main(String[] args) {
+        //todo 优化更优解
+    }
+
+    /**
+     * 暴力解法
+     * 直接计算左右的和比对
+     */
+    public int pivotIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int leftSum = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                leftSum += nums[j];
+            }
+            int rightSum = 0;
+            for (int j = i + 1; j < nums.length; j++) {
+                rightSum += nums[j];
+            }
+            if (leftSum == rightSum){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
