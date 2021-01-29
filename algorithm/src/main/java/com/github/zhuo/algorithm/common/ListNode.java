94d838bee58a968026aa1f1c06d332bdd04e2ec3
zhuyizhuo <2361883887@qq.com>
2021-01-29 22:04:58 +0800

    :tada:  新增 leetcode 题解
    :tada:  新增 leetcode 题解

--- algorithm/src/main/java/com/github/zhuo/algorithm/common/ListNode.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/common/ListNode.java
@@ -10,22 +10,6 @@ public class ListNode {
     public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public ListNode getNext() {
-        return next;
-    }
-
-    public void setNext(ListNode next) {
-        this.next = next;
-    }
-
     public static ListNode getListNode(int[] len){
         ListNode head = null,tail = null;
         for (int i = 0; i < len.length; i++) {
--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems38/CountAndSay.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems38/CountAndSay.java
@@ -8,17 +8,40 @@ package com.github.zhuo.algorithm.leetcode.problems38;
 public class CountAndSay {
 
     public static void main(String[] args) {
-
+        //TODO
+        System.out.println(countAndSay(3));
     }
 
-    public String countAndSay(int n) {
+    public static String countAndSay(int n) {
         if (n == 0){
             return "";
         }
-        StringBuffer sb = new StringBuffer("1");
+        String desc = "1";
         for (int i = 1; i < n; i++) {
-//            sb = say(sb);
+            desc = say(desc);
         }
-        return "";
+        return desc;
+    }
+
+    private static String say(String desc) {
+        char last = desc.charAt(0);
+        int lastCount = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < desc.length(); i++) {
+            if (last == desc.charAt(i)){
+                lastCount ++;
+            } else {
+                sb.append(lastCount).append(last);
+                lastCount = 1;
+                last = desc.charAt(i);
+            }
+            if (i == desc.length() - 1){
+                sb.append(lastCount).append(last);
+            }
+        }
+        if (sb.length() == 0){
+            sb.append(lastCount).append(last);
+        }
+        return sb.toString();
     }
 }
