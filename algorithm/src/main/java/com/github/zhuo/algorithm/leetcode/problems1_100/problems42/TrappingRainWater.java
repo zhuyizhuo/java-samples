c6247a4ac75181e788ea0850cd3385ef4b58adac
zhuyizhuo <2361883887@qq.com>
2021-03-13 22:27:30 +0800

    :pencil: 新增 leetcode 题解
    :pencil: 新增 leetcode 题解

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems1_100/problems42/TrappingRainWater.java
@@ -0,0 +1,38 @@
+package com.github.zhuo.algorithm.leetcode.problems1_100.problems42;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ * 接雨水
+ * 难度 困难
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 示例 1：
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ *
+ * 示例 2：
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ *
+ * 提示：
+ *
+ * n == height.length
+ * 0 <= n <= 3 * 10^4
+ * 0 <= height[i] <= 10^5
+ */
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public int trap(int[] height) {
+        Deque stack = new LinkedList();
+
+        return 0;
+    }
+}
