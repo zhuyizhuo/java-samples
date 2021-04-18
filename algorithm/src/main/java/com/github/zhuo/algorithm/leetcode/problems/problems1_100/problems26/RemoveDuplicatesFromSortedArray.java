86c93c12cf7c6a50afea64223a6f9883ecb40dc5
zhuyizhuo <2361883887@qq.com>
2021-04-18 22:38:43 +0800

    Update RemoveDuplicatesFromSortedArray.java

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems26/RemoveDuplicatesFromSortedArray.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems26/RemoveDuplicatesFromSortedArray.java
@@ -2,6 +2,11 @@ package com.github.zhuo.algorithm.leetcode.problems.problems1_100.problems26;
 
 /**
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array
+ * 删除有序数组中的重复项
+ * 难度 简单
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
  */
 public class RemoveDuplicatesFromSortedArray {
 
@@ -14,6 +19,10 @@ public class RemoveDuplicatesFromSortedArray {
         }
     }
 
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 81.45% 的用户
+     * 内存消耗： 40 MB , 在所有 Java 提交中击败了 91.89% 的用户
+     */
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0){
             return 0;
@@ -29,4 +38,5 @@ public class RemoveDuplicatesFromSortedArray {
         }
         return index;
     }
+
 }
