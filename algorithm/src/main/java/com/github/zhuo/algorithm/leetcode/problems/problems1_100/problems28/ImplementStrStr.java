85a95765762639179d29a038111b96d4e316b8e7
zhuyizhuo <2361883887@qq.com>
2021-04-22 16:20:11 +0800

    :pencil:  更新 doc
    :pencil:  更新 doc

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems28/ImplementStrStr.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems28/ImplementStrStr.java
@@ -4,6 +4,21 @@ package com.github.zhuo.algorithm.leetcode.problems.problems1_100.problems28;
  * https://leetcode.com/problems/implement-strstr/
  *
  * 实现 strStr()
+ *
+ * 难度 简单
+ * 
+ * 实现 strStr() 函数。
+ *
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
+ * 如果不存在，则返回  -1 。
+ *
+ * 说明：
+ *
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ *
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。
+ * 这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+ *
  */
 public class ImplementStrStr {
 
@@ -13,6 +28,8 @@ public class ImplementStrStr {
 
     /**
      * 双指针
+     * 执行用时： 181 ms , 在所有 Java 提交中击败了 5.01% 的用户
+     * 内存消耗： 39 MB , 在所有 Java 提交中击败了 5.09% 的用户
      *
      * 效率较差 需继续优化
      */
