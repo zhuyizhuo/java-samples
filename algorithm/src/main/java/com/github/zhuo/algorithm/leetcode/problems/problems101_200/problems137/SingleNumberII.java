2c37482dea9d320a2b3ab016513128afc89a87b8
zhuyizhuo <2361883887@qq.com>
2021-05-01 21:48:31 +0800

    :tada: 新增 leetcode 题解
    :tada: 新增 leetcode 题解

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems101_200/problems137/SingleNumberII.java
@@ -0,0 +1,46 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems101_200.problems137;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/single-number-ii/
+ * 只出现一次的数字 II
+ * 难度 中等
+ * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
+ *
+ */
+public class SingleNumberII {
+
+    public static void main(String[] args) {
+        System.out.println(singleNumber(new int[]{2,2,3,2}));
+        System.out.println(singleNumber(new int[]{0,1,0,1,0,1,99}));
+        System.out.println(singleNumber(new int[]{0,1,0,99,0,99,99}));
+    }
+
+    /**
+     * 执行用时： 2 ms , 在所有 Java 提交中击败了 67.50% 的用户
+     * 内存消耗： 38.3 MB , 在所有 Java 提交中击败了 35.59% 的用户
+     */
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 == nums.length){
+                return nums[i];
+            }
+            if (nums[i] == nums[i+1]){
+                i += 2;
+                continue;
+            } else {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+}
