bf89f6208fb264bf006674868043c0686e4ebad2
zhuyizhuo <2361883887@qq.com>
2021-05-06 10:10:38 +0800

    :tada: 新增 leetcode 题解
    :tada: 新增 leetcode 题解

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1701_1800/problems1720/DecodeXORedArray.java
@@ -0,0 +1,45 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1701_1800.problems1720;
+
+/**
+ * https://leetcode-cn.com/problems/decode-xored-array/
+ * 解码异或后的数组
+ *
+ * 难度 简单
+ * 未知 整数数组 arr 由 n 个非负整数组成。
+ *
+ * 经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。
+ * 例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
+ *
+ * 给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
+ *
+ * 请解码返回原数组 arr 。可以证明答案存在并且是唯一的。
+ *
+ * 提示：
+ *  2 <= n <= 10^4
+ *  encoded.length == n - 1
+ *  0 <= encoded[i] <= 10^5
+ *  0 <= first <= 10^5
+ */
+public class DecodeXORedArray {
+
+    public static void main(String[] args) {
+//        int[] decode = decode(new int[]{1, 2, 3}, 1);
+        int[] decode = decode(new int[]{6,2,7,3}, 4);
+        for (int i = 0; i < decode.length; i++) {
+            System.out.println(decode[i]);
+        }
+    }
+
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗： 39.6 MB , 在所有 Java 提交中击败了 17.40% 的用户
+     */
+    public static int[] decode(int[] encoded, int first) {
+        int[] resp = new int[encoded.length + 1];
+        resp[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            resp[i+1] = encoded[i] ^ resp[i];
+        }
+        return resp;
+    }
+}
