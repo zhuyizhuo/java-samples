bdc337ccda4d56d3fce52a393c9d59577e929db3
zhuyizhuo <2361883887@qq.com>
2021-05-21 21:15:57 +0800

    :pencil:  新增 leetcode 题目
    :pencil:  新增 leetcode 题目

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1001_1100/problems1035/UncrossedLines.java
@@ -0,0 +1,29 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1001_1100.problems1035;
+
+/**
+ * https://leetcode-cn.com/problems/uncrossed-lines/
+ * 不相交的线
+ * 难度 中等
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ *
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足：
+ *
+ *  nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ *
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ *
+ * 提示：
+ *
+ * 1 <= nums1.length <= 500
+ * 1 <= nums2.length <= 500
+ * 1 <= nums1[i], nums2[i] <= 2000
+ */
+public class UncrossedLines {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        //todo
+        return 0;
+    }
+}
