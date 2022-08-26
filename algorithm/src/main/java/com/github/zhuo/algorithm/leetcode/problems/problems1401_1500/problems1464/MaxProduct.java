d8a243c0a4254cad83642233ac1c626db445db11
zhuyizhuo <2361883887@qq.com>
2022-08-26 23:53:34 +0800

    新增题解 每日一题

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1401_1500/problems1464/MaxProduct.java
@@ -0,0 +1,29 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1401_1500.problems1464;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array/
+ *
+ * 给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。
+ *
+ * 请你计算并返回该式的最大值。
+ *
+ */
+public class MaxProduct {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 执行用时： 2 ms , 在所有 Java 提交中击败了 59.15% 的用户
+     * 内存消耗： 41 MB , 在所有 Java 提交中击败了 44.57% 的用户
+     * 通过测试用例： 104 / 104
+     */
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        int index = nums.length;
+        return (nums[index-2]-1)*(nums[index-1]-1);
+    }
+}
