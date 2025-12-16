4ae850e6dc6fa18c78cdbf45957eab648e9e0ca9
zhuyizhuo <zhu.yizhuo@163.com>
2025-12-16 23:27:51 +0800

    新增题解37

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems37/Solution37.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/problems37/Solution37.java
@@ -1,5 +1,8 @@
 package com.github.zhuo.algorithm.leetcode.problems.problems1_100.problems37;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 编写一个程序，通过填充空格来解决数独问题。
  *
@@ -26,7 +29,43 @@ public class Solution37 {
 
     }
 
-    public void solveSudoku(char[][] board) {
+    private boolean[][] line = new boolean[9][9];
+    private boolean[][] column = new boolean[9][9];
+    private boolean[][][] block = new boolean[3][3][9];
+    private boolean valid = false;
+    private List<int[]> spaces = new ArrayList<int[]>();
 
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    spaces.add(new int[]{i, j});
+                } else {
+                    int digit = board[i][j] - '0' - 1;
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                }
+            }
+        }
+
+        dfs(board, 0);
     }
+
+    public void dfs(char[][] board, int pos) {
+        if (pos == spaces.size()) {
+            valid = true;
+            return;
+        }
+
+        int[] space = spaces.get(pos);
+        int i = space[0], j = space[1];
+        for (int digit = 0; digit < 9 && !valid; ++digit) {
+            if (!line[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
+                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                board[i][j] = (char) (digit + '0' + 1);
+                dfs(board, pos + 1);
+                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
+            }
+        }
+    }
+}
 }
