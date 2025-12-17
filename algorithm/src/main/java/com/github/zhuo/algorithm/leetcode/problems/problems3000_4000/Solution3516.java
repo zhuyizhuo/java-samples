15256f61155f51189f0bcce19b96b7ff07a790d7
一卓 <zhu.yizhuo@163.com>
2025-12-17 18:10:16 +0800

    Update Solution3516.java

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3000_4000/Solution3516.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3000_4000/Solution3516.java
@@ -1,11 +1,11 @@
-package com.github.zhuo.algorithm.leetcode.problems.problems3000_4000.problems3516;
+package com.github.zhuo.algorithm.leetcode.problems.problems3000_4000;
 
 
 /**
  * @description:
  * @author: zhuo
  */
-public class FindClosest {
+public class Solution3516 {
 
     public static void main(String[] args) {
         System.out.println(findClosest(2,7,4));
