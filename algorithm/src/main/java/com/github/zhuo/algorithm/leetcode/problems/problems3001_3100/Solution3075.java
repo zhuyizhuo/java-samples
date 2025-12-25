a41eac1ead7f2ccee08eab917003e44ecb0f1aee
zhuyizhuo <zhu.yizhuo@163.com>
2025-12-25 23:30:43 +0800

    add solution

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3001_3100/Solution3075.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3001_3100/Solution3075.java
@@ -1,5 +1,7 @@
 package com.github.zhuo.algorithm.leetcode.problems.problems3001_3100;
 
+import java.util.Arrays;
+
 public class Solution3075 {
 
     public static void main(String[] args) {
@@ -7,6 +9,17 @@ public class Solution3075 {
     }
 
     public long maximumHappinessSum(int[] happiness, int k) {
-        return 1;
-    }
-}
+        int n=happiness.length;
+        Arrays.sort(happiness);
+        int i=n-1,turn=0;
+        long sum=0;
+        while(turn<k&&i>=0){
+            if(happiness[i]-turn<=0){
+                break;
+            }
+            sum+=happiness[i]-turn;
+            i--;
+            turn++;
+        }
+        return sum;
+    }}
