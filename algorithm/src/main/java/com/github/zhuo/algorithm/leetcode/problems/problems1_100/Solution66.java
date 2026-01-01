68f2cfa1dba8d7d041e60a664222e5d544136769
zhuyizhuo <zhu.yizhuo@163.com>
2026-01-01 13:25:29 +0800

    Update Solution66.java

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/Solution66.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/Solution66.java
@@ -11,6 +11,9 @@ public class Solution66 {
         System.out.println(plusOne(new int[]{9,9,9}));
     }
 
+    /**
+     * beats 100%
+     */
     public static int[] plusOne(int[] digits) {
         boolean carry = false;
         for (int i = digits.length - 1; i >= 0; i--) {
@@ -36,5 +39,24 @@ public class Solution66 {
             return rtn;
         }
         return digits;
+    }
+
+    /**
+     * beats 100%
+     */
+    public int[] plusOne1(int[] digits) {
+        int res=1,n=digits.length;
+        for(int i=n-1;i>=0;i--) {
+            res+=digits[i];
+            digits[i]=res%10;
+            res/=10;
+            if(res==0) return digits;
+        }
+        int[] ans=new int[n+1];
+        ans[0]=1;
+        for(int i=1;i<=n;i++) {
+            ans[i]=digits[i-1];
+        }
+        return ans;
     }
 }
