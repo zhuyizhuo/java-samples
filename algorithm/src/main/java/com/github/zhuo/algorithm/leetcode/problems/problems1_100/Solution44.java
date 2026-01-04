66067f95a6a55eb1c845e3824020c516c19c98e5
一卓 <zhu.yizhuo@163.com>
2026-01-04 14:58:20 +0800

    create solutions

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/Solution44.java
@@ -0,0 +1,66 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1_100;
+
+/**
+ * 通配符匹配
+ * hard
+ *
+ * 给你一个输入字符串 (s) 和一个字符模式 (p) ，请你实现一个支持 '?' 和 '*' 匹配规则的通配符匹配：
+ * '?' 可以匹配任何单个字符。
+ * '*' 可以匹配任意字符序列（包括空字符序列）。
+ * 判定匹配成功的充要条件是：字符模式必须能够 完全匹配 输入字符串（而不是部分匹配）。
+ */
+public class Solution44 {
+    /**
+     * beats 97.48%
+     */
+    public boolean isMatch(String s, String p) {
+        int sRight = s.length(), pRight = p.length();
+        while (sRight > 0 && pRight > 0 && p.charAt(pRight - 1) != '*') {
+            if (charMatch(s.charAt(sRight - 1), p.charAt(pRight - 1))) {
+                --sRight;
+                --pRight;
+            } else {
+                return false;
+            }
+        }
+
+        if (pRight == 0) {
+            return sRight == 0;
+        }
+
+        int sIndex = 0, pIndex = 0;
+        int sRecord = -1, pRecord = -1;
+
+        while (sIndex < sRight && pIndex < pRight) {
+            if (p.charAt(pIndex) == '*') {
+                ++pIndex;
+                sRecord = sIndex;
+                pRecord = pIndex;
+            } else if (charMatch(s.charAt(sIndex), p.charAt(pIndex))) {
+                ++sIndex;
+                ++pIndex;
+            } else if (sRecord != -1 && sRecord + 1 < sRight) {
+                ++sRecord;
+                sIndex = sRecord;
+                pIndex = pRecord;
+            } else {
+                return false;
+            }
+        }
+
+        return allStars(p, pIndex, pRight);
+    }
+
+    public boolean allStars(String str, int left, int right) {
+        for (int i = left; i < right; ++i) {
+            if (str.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean charMatch(char u, char v) {
+        return u == v || v == '?';
+    }
+}
---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/Solution48.java
@@ -0,0 +1,26 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1_100;
+
+/**
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ *
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ */
+public class Solution48 {
+    /**
+     * beats 100%
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+}
