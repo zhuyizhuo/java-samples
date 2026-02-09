0752e248b560f1108cc677a9ddaa223becc483eb
一卓 <zhu.yizhuo@163.com>
2026-02-09 10:26:04 +0800

    create Solution1382

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1301_1400/Solution1382.java
@@ -0,0 +1,48 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1301_1400;
+
+import com.github.zhuo.algorithm.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 将二叉搜索树变平衡
+ *
+ * 给你一棵二叉搜索树，请你返回一棵 平衡后 的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。如果有多种构造方法，请你返回任意一种。
+ *
+ * 如果一棵二叉搜索树中，每个节点的两棵子树高度差不超过 1 ，我们就称这棵二叉搜索树是 平衡的 。
+ */
+public class Solution1382 {
+
+    List<Integer> inorderSeq = new ArrayList<Integer>();
+
+    /**
+     * beats 100%
+     */
+    public TreeNode balanceBST(TreeNode root) {
+        getInorder(root);
+        return build(0, inorderSeq.size() - 1);
+    }
+
+    public void getInorder(TreeNode o) {
+        if (o.left != null) {
+            getInorder(o.left);
+        }
+        inorderSeq.add(o.val);
+        if (o.right != null) {
+            getInorder(o.right);
+        }
+    }
+
+    public TreeNode build(int l, int r) {
+        int mid = (l + r) >> 1;
+        TreeNode o = new TreeNode(inorderSeq.get(mid));
+        if (l <= mid - 1) {
+            o.left = build(l, mid - 1);
+        }
+        if (mid + 1 <= r) {
+            o.right = build(mid + 1, r);
+        }
+        return o;
+    }
+}
