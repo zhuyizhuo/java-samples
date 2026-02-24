e7a3d2d3f663d14a11b389a96bcd6355be826c79
一卓 <zhu.yizhuo@163.com>
2026-02-24 11:10:18 +0800

    Create Solution1022.java

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1001_1100/Solution1022.java
@@ -0,0 +1,34 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1001_1100;
+
+import com.github.zhuo.algorithm.common.TreeNode;
+
+/**
+ * 从根到叶的二进制数之和
+ * easy
+ *
+ * 给出一棵二叉树，其上每个结点的值都是 0 或 1 。每一条从根到叶的路径都代表一个从最高有效位开始的二进制数。
+ *
+ * 例如，如果路径为 0 -> 1 -> 1 -> 0 -> 1，那么它表示二进制数 01101，也就是 13 。
+ * 对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。
+ *
+ * 返回这些数字之和。题目数据保证答案是一个 32 位 整数。
+ */
+public class Solution1022 {
+    /**
+     * beats 100%
+     */
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int val) {
+        if (root == null) {
+            return 0;
+        }
+        val = (val << 1) | root.val;
+        if (root.left == null && root.right == null) {
+            return val;
+        }
+        return dfs(root.left, val) + dfs(root.right, val);
+    }
+}
