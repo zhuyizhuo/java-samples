40d2f5ba08f6db6079a8fcc9ecec4d24158816b0
一卓 <zhu.yizhuo@163.com>
2026-02-28 10:28:28 +0800

    Update Solution190.java

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems101_200/Solution190.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems101_200/Solution190.java
@@ -1,5 +1,11 @@
 package com.github.zhuo.algorithm.leetcode.problems.problems101_200;
 
+/**
+ * 颠倒二进制位
+ * easy
+ *
+ * 颠倒给定的 32 位有符号整数的二进制位。
+ */
 public class Solution190 {
 
     public static void main(String[] args) {
@@ -7,8 +13,15 @@ public class Solution190 {
         System.out.println(s);
     }
 
+    /**
+     * beats 100%
+     */
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
     }
-
 }
