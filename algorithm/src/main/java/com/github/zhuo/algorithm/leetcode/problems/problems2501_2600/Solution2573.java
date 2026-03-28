df50425d4b31f7940824006948d68d8919c762d3
zhuyizhuo <zhu.yizhuo@163.com>
2026-03-28 22:37:30 +0800

    Create Solution2573.java

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems2501_2600/Solution2573.java
@@ -0,0 +1,63 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems2501_2600;
+
+/**
+ * 找出对应 LCP 矩阵的字符串
+ * hard
+ *
+ * 对任一由 n 个小写英文字母组成的字符串 word ，我们可以定义一个 n x n 的矩阵，并满足：
+ *
+ * lcp[i][j] 等于子字符串 word[i,...,n-1] 和 word[j,...,n-1] 之间的最长公共前缀的长度。
+ * 给你一个 n x n 的矩阵 lcp 。返回与 lcp 对应的、按字典序最小的字符串 word 。如果不存在这样的字符串，则返回空字符串。
+ *
+ * 对于长度相同的两个字符串 a 和 b ，如果在 a 和 b 不同的第一个位置，字符串 a 的字母在字母表中出现的顺序先于 b 中的对应字母，则认为字符串 a 按字典序比字符串 b 小。例如，"aabd" 在字典上小于 "aaca" ，因为二者不同的第一位置是第三个字母，而 'b' 先于 'c' 出现。
+ */
+public class Solution2573 {
+
+    /**
+     * beats 100%
+     */
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+        char[] word = new char[n];
+        char current = 'a';
+
+        // 依次从 'a' 到 'z' 开始构造字符串
+        for (int i = 0; i < n; i++) {
+            if (word[i] == 0) {
+                if (current > 'z') {
+                    return "";
+                }
+                word[i] = current;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = word[i];
+                    }
+                }
+                current++;
+            }
+        }
+
+        // 验证构造的字符串是否满足 LCP 矩阵要求
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (word[i] != word[j]) {
+                    if (lcp[i][j] != 0) {
+                        return "";
+                    }
+                } else {
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) {
+                            return "";
+                        }
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
+                            return "";
+                        }
+                    }
+                }
+            }
+        }
+
+        return new String(word);
+    }
+}
