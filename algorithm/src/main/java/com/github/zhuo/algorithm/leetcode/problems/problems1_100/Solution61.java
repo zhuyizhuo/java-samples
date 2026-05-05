bc9f79bdb3356224c625b03fe15cc4e3126731a3
zhuyizhuo <zhu.yizhuo@163.com>
2026-05-05 15:42:53 +0800

    create solution61

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1_100/Solution61.java
@@ -0,0 +1,37 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1_100;
+
+import com.github.zhuo.algorithm.common.ListNode;
+
+/**
+ * 旋转链表
+ *
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ */
+public class Solution61 {
+
+    /**
+     * beats 100%
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            n++;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+}
