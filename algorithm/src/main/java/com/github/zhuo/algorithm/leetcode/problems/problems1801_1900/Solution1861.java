ae52ed98cf15905d7919388af719a8c82862d2fc
一卓 <zhu.yizhuo@163.com>
2026-05-06 10:37:00 +0800

    create Solution1861

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1801_1900/Solution1861.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1801_1900/Solution1861.java
@@ -72,4 +72,30 @@ public class Solution1861 {
         }
         return resp;
     }
+
+    /**
+     * 2026-5-6
+     * beat 90.2%
+     */
+    public char[][] rotateTheBox1(char[][] boxGrid) {
+        int m=boxGrid.length;
+        int n=boxGrid[0].length;
+        char[][]ch=new char[n][m];
+        for(char[]row:ch){
+            Arrays.fill(row,'.');
+        }
+        for(int i=0;i<m;i++){
+            int empty=n-1;
+            for(int j=n-1;j>=0;j--){
+                if(boxGrid[i][j]=='*'){
+                    ch[j][m-1-i]='*';
+                    empty=j-1;
+                }else if(boxGrid[i][j]=='#'){
+                    ch[empty][m - 1 - i] = '#';
+                    empty--;
+                }
+            }
+        }
+        return ch;
+    }
 }
