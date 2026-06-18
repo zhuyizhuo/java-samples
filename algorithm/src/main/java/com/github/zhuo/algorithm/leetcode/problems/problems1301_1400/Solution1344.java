6d2320a876d39856b6b33c75535bca2cc3ed3633
一卓 <zhu.yizhuo@163.com>
2026-06-18 10:13:34 +0800

    create Solution1344

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1301_1400/Solution1344.java
@@ -0,0 +1,23 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1301_1400;
+
+/**
+ * 时钟指针的夹角
+ *
+ * 给你两个数 hour 和 minutes 。请你返回在时钟上，由给定时间的时针和分针组成的较小角的角度（60 单位制）。
+ */
+public class Solution1344 {
+
+    /**
+     * beats 100%
+     */
+    public double angleClock(int hour, int minutes) {
+        int oneMinAngle = 6;
+        int oneHourAngle = 30;
+
+        double minutesAngle = oneMinAngle * minutes;
+        double hourAngle = (hour % 12 + minutes / 60.0) * oneHourAngle;
+
+        double diff = Math.abs(hourAngle - minutesAngle);
+        return Math.min(diff, 360 - diff);
+    }
+}
