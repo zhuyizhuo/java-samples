606b544239483991b9ab4c43ae3be2484065e424
zhuyizhuo <zhu.yizhuo@163.com>
2026-06-21 01:00:23 +0800

    create solution1732

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1701_1800/Solution1732.java
@@ -0,0 +1,24 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems1701_1800;
+
+/**
+ * 找到最高海拔
+ * easy
+ * 有一个自行车手打算进行一场公路骑行，这条路线总共由 n + 1 个不同海拔的点组成。自行车手从海拔为 0 的点 0 开始骑行。
+ *
+ * 给你一个长度为 n 的整数数组 gain ，其中 gain[i] 是点 i 和点 i + 1 的 净海拔高度差（0 <= i < n）。请你返回 最高点的海拔 。
+ *
+ */
+public class Solution1732 {
+
+    /**
+     * beats 100%
+     */
+    public int largestAltitude(int[] gain) {
+        int ans = 0, sum = 0;
+        for (int x : gain) {
+            sum += x;
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
