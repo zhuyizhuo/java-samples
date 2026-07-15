321907feedeb25aac047785f8378c40fe6963ae7
zhuyizhuo <zhu.yizhuo@163.com>
2026-07-15 17:28:58 +0800

    create Solution3658

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3601_3700/Solution3658.java
@@ -0,0 +1,15 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems3601_3700;
+
+/**
+ * 奇数和与偶数和的最大公约数
+ * easy
+ */
+public class Solution3658 {
+
+    /**
+     * beats 100%
+     */
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
