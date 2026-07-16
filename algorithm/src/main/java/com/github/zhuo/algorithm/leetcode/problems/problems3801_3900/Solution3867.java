58f14fa38099eb184965f348a3b3d698a491b829
zhuyizhuo <zhu.yizhuo@163.com>
2026-07-16 23:45:39 +0800

    create Solution3867

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3801_3900/Solution3867.java
@@ -0,0 +1,50 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems3801_3900;
+
+import java.util.Arrays;
+
+/**
+ * 数对的最大公约数之和
+ */
+public class Solution3867 {
+
+    /**
+     * beats 100%
+     */
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+
+        int[] mx = new int[n];
+        int prefixMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; ++i) {
+            prefixMax = Math.max(prefixMax, nums[i]);
+            mx[i] = prefixMax;
+        }
+
+        int[] prefixGcd = new int[n];
+        for (int i = 0; i < n; ++i) {
+            prefixGcd[i] = gcd(nums[i], mx[i]);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            ans += gcd(prefixGcd[left], prefixGcd[right]);
+            ++left;
+            --right;
+        }
+
+        return ans;
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
+    }
+}
