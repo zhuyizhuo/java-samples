0ad403a116afe40e503f6d7a3aca7f13e1b56342
zhuyizhuo <zhu.yizhuo@163.com>
2026-07-26 16:50:51 +0800

    create Solution628

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems601_700/Solution628.java
@@ -0,0 +1,48 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems601_700;
+
+/**
+ * 三个数的最大乘积
+ */
+public class Solution628 {
+
+    /**
+     * beats 99.61%
+     */
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < max3) {
+
+            } else if (nums[i] > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            } else if (nums[i] > max2) {
+                max3 = max2;
+                max2 = nums[i];
+            } else if (nums[i] > max3) {
+                max3 = nums[i];
+            }
+            if (nums[i] > min2) {
+
+            } else if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        int number1 = max1 * max2 * max3;
+        int number2 = max1 * min2 * min1;
+        if (number1 > number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+
+    }
+}
