a3cf157c014d3ac65da07e61661add84a8d4114b
zhuyizhuo <zhu.yizhuo@163.com>
2026-07-27 20:35:19 +0800

    create Solution1464

--- algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1401_1500/Solution1464.java
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems1401_1500/Solution1464.java
@@ -26,4 +26,40 @@ public class Solution1464 {
         int index = nums.length;
         return (nums[index-2]-1)*(nums[index-1]-1);
     }
+
+    /**
+     * beats 77.19%
+     */
+    public int maxProduct1(int[] nums) {
+        int first=0;
+        int second=0;
+        for(int i=0;i<nums.length;i++){
+            second=Math.max(second,nums[i]);
+            if(second>first){
+                int temp=second;
+                second=first;
+                first=temp;
+            }
+        }
+        return (first-1)*(second-1);
+    }
+
+    /**
+     * beats 100%
+     */
+    public int maxProduct2(int[] nums) {
+        int max1=0;
+        int max2=0;
+        for(int num:nums){
+            if(num>max1){
+                max2=max1;
+                max1=num;
+            }
+            else if(num>max2){
+                max2=num;
+            }
+        }
+        return (max1 - 1)*(max2 -1);
+
+    }
 }
