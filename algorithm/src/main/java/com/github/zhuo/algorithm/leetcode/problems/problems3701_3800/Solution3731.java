763b608e9b1bc656890e668c214ba1a15b48833d
zhuyizhuo <zhu.yizhuo@163.com>
2026-08-04 20:26:02 +0800

    create Solution3731

---
+++ algorithm/src/main/java/com/github/zhuo/algorithm/leetcode/problems/problems3701_3800/Solution3731.java
@@ -0,0 +1,28 @@
+package com.github.zhuo.algorithm.leetcode.problems.problems3701_3800;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 找出缺失的元素
+ */
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        //min,max
+        //shuzu
+        int min = 100;int max = 0;
+        boolean[] f = new boolean[101];
+        for(int x:nums){
+            f[x] = true;
+            min = Math.min(min,x);
+            max = Math.max(max,x);
+        }
+        for(int i = min+1;i<max;i++){
+            if(!f[i]) res.add(i);
+        }
+        return res;
+
+
+    }
+}
